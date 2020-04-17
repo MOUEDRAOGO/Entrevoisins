95ce3d43bcaaa98ac0f295a01b2bb73243ed5cd2
mouedraogo <mouedraogo@yahoo.com>
2020-04-17 17:38:17 +0200

    1704201738

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/PageDetailVoisinActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/PageDetailVoisinActivity.java
@@ -1,7 +1,10 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.openclassrooms.entrevoisins.R;
 
@@ -11,5 +14,11 @@ public class PageDetailVoisinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_detail_voisin);
+
+        //Recuperation de la vue
+        final Intent intent = getIntent();
+        View pageDetailVoisinView = (TextView) findViewById(R.id.activity_page_detail_voisin);
+        //remplissage de la vue selectionnee avec la bonne data provenant de l'objet
+        pageDetailVoisinView.setText(neighbour.getId());
     }
 }
