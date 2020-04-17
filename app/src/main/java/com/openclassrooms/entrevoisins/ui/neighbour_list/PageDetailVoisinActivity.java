d664a83197977be5d69c279d01c3864edd768766
mouedraogo <mouedraogo@yahoo.com>
2020-04-17 12:56:19 +0200

    first commit

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/PageDetailVoisinActivity.java
@@ -0,0 +1,15 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.openclassrooms.entrevoisins.R;
+
+public class PageDetailVoisinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_detail_voisin);
+    }
+}
